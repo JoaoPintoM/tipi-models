c556fb85384ff634f196cc74dbb1e444f181719e
Axel Glorieux <axel.glorieux@gmail.com>
2014-12-05 12:33:40 +0100

    fix dep

---
+++ City.js
@@ -0,0 +1,26 @@
+module.exports = function(mongoose) {
+
+	var CitySchema = new mongoose.Schema({
+        zip: String,
+        name_fr: String,
+        name_nl: String,
+        name_en: String,
+        search_string_fr: String,
+        search_string_nl: String,
+        search_string_en: String,
+        borders: Array,
+        center: Array,
+        parent_zip: String,
+        order_id: Number,
+        website_fr: String,
+        website_nl: String,
+        website_en: String,
+        wikipedia_fr: String,
+        wikipedia_nl: String,
+        wikipedia_en: String
+	})
+
+	var City = mongoose.model('City', CitySchema);
+
+	return City
+}
