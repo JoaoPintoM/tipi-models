ba9916f8f194ecbc88a37f89f6c3f3310c975ce8
Maxime Sahroui <me@maximesahroui.be>
2015-03-11 14:26:00 +0100

    adding notifications for MODELS

--- Notification.js
+++ Notification.js
@@ -1,16 +1,39 @@
 'use strict';
-module.exports = function(mongoose) {
-	
-	
-	var notifSchema = new mongoose.Schema({
+
+module.exports = function (mongoose) {
+
+	var notificationSchema = new mongoose.Schema({
 		user: {
-			type: Schema.ObjectId,
+			type: mongoose.Schema.ObjectId,
 			ref: 'User'
-		}
+		},
+    zip: {
+      type: [Number],
+      min: 1000,
+      max: 9999
+    },
+    frequency: {
+      type: String,
+      enum: ['daily', 'weekly'],
+      default: 'daily'
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now
+    },
 	});
-	
-	var Notification = mongoose.model('Notification', notifSchema);
-	
+
+  notificationSchema.path('zip').validate(function (zip) {
+    if (zip.length <= 0) return false;
+    return true;
+  }, 'Zip cannot be empty');
+
+	var Notification = mongoose.model('Notification', notificationSchema);
+
 	return Notification;
 }
-	
+
