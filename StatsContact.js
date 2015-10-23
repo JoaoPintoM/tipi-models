ee3f433ae42a207deeb0b4ba0280229d45d9482c
JoaoPintoM <pinto.joao@outlook.com>
2015-10-23 17:28:25 +0200

    test commit

--- StatsContact.js
+++ StatsContact.js
@@ -1,46 +1,49 @@
 'use strict';
 
-module.exports = function (mongoose) {
+module.exports = function(mongoose) {
 
 	var statsContactSchema = new mongoose.Schema({
 		user: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User'
-    },
-    recipientUser: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User'
-    },
-    recipientAgency: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'Agency'
-    },
-    recipientType: {
-      type: String,
-      enum: [ 'user', 'agency' ]
-    },
-    sendTo: {
-      type: String
-    },
-    message: {
-      type: String,
-    },
-    estate: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'Estate'
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now
-    },
-    updatedAt: {
-      type: Date,
-      default: Date.now
-    },
+			type: mongoose.Schema.ObjectId,
+			ref: 'User'
+		},
+		recipientUser: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'User'
+		},
+		recipientAgency: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'Agency'
+		},
+		recipientType: {
+			type: String,
+			enum: ['user', 'agency', 'ads']
+		},
+		sendTo: {
+			type: String
+		},
+		message: {
+			type: String,
+		},
+		estate: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'Estate'
+		},
+		ad: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'Advert'
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now
+		},
+		updatedAt: {
+			type: Date,
+			default: Date.now
+		},
 	});
 
 	var StatsContact = mongoose.model('StatsContact', statsContactSchema);
 
 	return StatsContact;
 }
-
