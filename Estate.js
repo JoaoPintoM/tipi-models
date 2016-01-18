3ea4e659d1b8f7e48ae998f3b51b93b1b2934bb6
JoaoPintoM <pinto.joao@outlook.com>
2016-01-18 09:59:18 +0100

    go

--- Estate.js
+++ Estate.js
@@ -237,6 +237,7 @@ module.exports = function(mongoose, request, translator) {
   })
 
   EstateSchema.path('zip').validate(function(val) {
+  	console.log('valZip:'  + val);
     var ziputils = require('./ziputils')
     return ziputils.isZip(val)
   }, 'InvalidZip');
